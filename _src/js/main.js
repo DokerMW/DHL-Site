842b203a4c43fb88ebb940e0756b674a2ac43ca4
DokerMW <abddoker@gmail.com>
2021-10-13 21:51:32 +0500

    1

---
+++ _src/js/main.js
@@ -0,0 +1,8 @@
+//@prepros-append forms.js
+//@prepros-append functions.js
+//@prepros-append script.js
+
+// @@include('files/regular.js', {})
+// @@include('files/sliders.js', {})
+// @@include('files/scroll.js', {})
+
