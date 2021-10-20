969eb4b6f9b5ed5f581a3f0de6ea275712921b0b
DokerMW <86104259+DokerMW@users.noreply.github.com>
2021-10-20 17:44:20 +0500

    Add files via upload

---
+++ _src/js/main.js
@@ -0,0 +1,9 @@
+//@prepros-append forms.js
+//@prepros-append functions.js
+//@prepros-append burger_js.js
+//@prepros-append dynamic_adapt.js
+
+// @@include('files/regular.js', {})
+// @@include('files/sliders.js', {})
+// @@include('files/scroll.js', {})
+
